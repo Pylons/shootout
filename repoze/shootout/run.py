f23c98ca600d3610cbbf25b305bf300f6f315b96
Chris McDonough <chrism@agendaless.com>
2010-07-22 08:16:30 +0000

    use pypi

--- repoze/shootout/run.py
+++ repoze/shootout/run.py
@@ -1,6 +1,5 @@
 def make_app(global_config, **kw):
     # paster app config callback
-    from repoze.bfg.router import make_app
     from repoze.shootout.models import get_root
     from repoze.shootout.models import initialize_sql
     db = kw.get('db')
@@ -8,7 +7,10 @@ def make_app(global_config, **kw):
         raise ValueError('shootout.ini requires a db section '
                          '(the SQLAlchemy db URI)')
     initialize_sql(db)
-    import repoze.shootout
-    app = make_app(get_root, repoze.shootout)
-    return app
+    from repoze.bfg.configuration import Configurator
+    config = Configurator(root_factory=get_root)
+    config.begin()
+    config.load_zcml('repoze.shootout:configure.zcml')
+    config.end()
+    return config.make_wsgi_app()
 
