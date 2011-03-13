b9d2ad22762426374b629b8779a5f7f1e31f83fb
Łukasz Fidosz <virhilo@gmail.com>
2011-03-13 20:15:20 +0100

    added pyramid_simpleform to dependences

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
     'repoze.tm2>=1.0b1', # default_commit_veto
     'zope.sqlalchemy',
     'WebError',
-    'formencode',
+    'pyramid_simpleform',
     ]
 
 if sys.version_info[:3] < (2,5,0):
